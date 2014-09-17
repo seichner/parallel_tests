a22895486ec4b983f4a6111f8e80ec81fa248bf2
seichner <mail@sebastian-eichner.de>
2014-09-17 11:43:37 +0200

    Update io.rb

--- lib/parallel_tests/gherkin/io.rb
+++ lib/parallel_tests/gherkin/io.rb
@@ -8,8 +8,8 @@ module ParallelTests
         if path_or_io.respond_to?(:write)
           path_or_io
         else # its a path
-          #File.open(path_or_io, 'w').close # clean out the file
-          Kernel.puts "not opening a new file (in gherkin)"
+          File.open(path_or_io, 'w').close # clean out the file
+          Kernel.puts "cleaning file (in gherkin)"
           file = File.open(path_or_io, 'a')
 
           at_exit do
